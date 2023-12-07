f8a199296d5691ae3a6efd55651bfce26fe8c976
warriebikeye <153061110+warriebikeye@users.noreply.github.com>
2023-12-07 02:19:44 -0800

    Add files via upload

--- clone/src/openai.js
+++ clone/src/openai.js
@@ -1,7 +1,7 @@
 const OpenAI = require ("openai");
 
 const openai = new OpenAI({
-    apiKey: "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",dangerouslyAllowBrowser: true // This is also the default, can be omitted
+    apiKey: "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",dangerouslyAllowBrowser: true // This is also the default, can be omitted
   });
 
 export async function sendMsgToOpenAI(message){
